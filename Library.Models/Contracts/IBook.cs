d2614498d1f36cfa8c1d3d611f0a8f749bb15bde
Hrisi.Stanoeva <hristina.stanoeva@gmail.com>
2019-08-05 13:12:33 +0300

    Book class modified

--- Library.Models/Contracts/IBook.cs
+++ Library.Models/Contracts/IBook.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Library.Models.Enums;
 
 namespace Library.Models.Contracts
 {
     public interface IBook
     {
+        string Author { get; }
+        string Title { get; }
+        string ISBN { get; }
+        string Genre { get; }
+        string Publisher { get; }
+        int Year { get; }
+        int Rack { get; }
+        BookStatus Status { get; set; }
     }
 }
--- Library.Models/Models/Book.cs
+++ Library.Models/Models/Book.cs
@@ -1,24 +1,121 @@
 ﻿using Library.Models.Contracts;
 using Library.Models.Enums;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Library.Models.Models
 {
     public class Book : IBook
     {
-        public Book(int id,
-                    string author,
-                    string title,
-                    string isbn,
-                    string subject,
-                    string publisher,
-                    int year,
-                    int rack,
-                    BookStatus status)
-        {
+        private static int currentId = 0;
+        private string author;
+        private string title;
+        private string genre;
+        private string publisher;
+        private int year;
 
+        public Book(string author, string title, string isbn, string genre, string publisher, int year, int rack, BookStatus status)
+        {
+            this.ID = currentId++;
+            this.Author = author;
+            this.Title = title;
+            this.ISBN = isbn;
+            this.Genre = genre;
+            this.Publisher = publisher;
+            this.Year = year;
+            this.Rack = rack;
+            this.Status = status;
         }
+
+        public int ID { get; }
+
+        public string Author
+        {
+            get
+            {
+                return this.author;
+            }
+            set
+            {
+                if (value.Length < 1 || value.Length > 40)
+                {
+                    throw new ArgumentOutOfRangeException("The author name should be between 1 and 40 symbols!");
+                }
+                this.author = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                if (value.Length < 1 || value.Length > 40)
+                {
+                    throw new ArgumentOutOfRangeException("The title should be between 1 and 40 symbols!");
+                }
+                this.title = value;
+            }
+        }
+
+        public string ISBN { get; }
+
+        public string Genre
+        {
+            get
+            {
+                return this.genre;
+            }
+            set
+            {
+                if (value.Length < 1 || value.Length > 20)
+                {
+                    throw new ArgumentOutOfRangeException("The genre should be between 1 and 20 symbols!");
+                }
+                this.genre = value;
+            }
+        }
+
+        public string Publisher
+        {
+            get
+            {
+                return this.publisher;
+            }
+            set
+            {
+                if (value.Length < 1 || value.Length > 40)
+                {
+                    throw new ArgumentOutOfRangeException("The publisher name should be between 1 and 40 symbols!");
+                }
+                this.publisher = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                if (value < 1 || value > 2019)
+                {
+                    throw new ArgumentOutOfRangeException("The publication year should be between 1 and 2019");
+                }
+                this.year = value;
+            }
+        }
+
+        public int Rack { get; }
+
+        public BookStatus Status { get; set; }
+
+        DateTime CheckoutDate { get; set; }
+        DateTime DueDate { get; set; }
+        DateTime ResevedDate { get; set; }
     }
 }
