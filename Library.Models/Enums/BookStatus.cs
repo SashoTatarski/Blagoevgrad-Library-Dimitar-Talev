436f1143033d129dd6c4e8217ab6c266785f1408
Hrisi.Stanoeva <hristina.stanoeva@gmail.com>
2019-08-05 12:24:48 +0300

    Added Book class and interface

---
+++ Library.Models/Enums/BookStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Models.Enums
+{
+    public enum BookStatus
+    {
+        Available,
+        Checkedout,
+        Reserved
+    }
+}
---
+++ Library.Models/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using Library.Models.Contracts;
+using Library.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Models.Models
+{
+    public class Book : IBook
+    {
+        public Book(int id,
+                    string author,
+                    string title,
+                    string isbn,
+                    string subject,
+                    string publisher,
+                    int year,
+                    int rack,
+                    BookStatus status)
+        {
+
+        }
+    }
+}
