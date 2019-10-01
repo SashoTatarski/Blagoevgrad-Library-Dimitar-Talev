710d8d0655ae1b407f7e34b7350a0b33a2812e52
Hrisi.Stanoeva <hristina.stanoeva@gmail.com>
2019-10-01 10:51:20 +0300

    DataSeeder added

---
+++ Library.Databse/Seed/Seeder.cs
@@ -0,0 +1,49 @@
+﻿using Library.Models.Enums;
+using Library.Models.Models;
+using System;
+using System.Linq;
+
+namespace Library.Database.Seed
+{
+    public static class Seeder
+    {
+        public static void Init(LibraryContext context)
+        {
+            SeedRoles(context);
+            SeedAdmin(context);
+        }
+
+        private static void SeedRoles(LibraryContext context)
+        {
+            if (context.Roles.Any())
+                return;
+
+            var roleNames = new[] { "user", "admin" };
+            context.Roles.AddRange(
+                roleNames.Select(name => new Role { RoleName = name })
+            );
+            context.SaveChanges();
+        }
+
+        private static void SeedAdmin(LibraryContext context)
+        {
+            // check if teams are already in the db
+            if (context.Users.Any(u => u.Username == "admin"))
+                return;
+
+            var adminRole = context.Roles.Where(r => r.RoleName == "admin").FirstOrDefault();
+
+            context.Users.Add(new User
+            {
+                Username = "admin",
+                HashPassword = "M8GXPIckpkXfuYusx9e1xzZt4eTqCim2gtd5xmsHk4ds+BePHFMN9+DItyR7IiaJ",
+                RoleId = adminRole.Id,
+                MembershipStartDate = DateTime.Today,
+                MembershipEndDate = DateTime.Today.AddYears(1),
+                Status = AccountStatus.Active
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
--- Library.Web/Program.cs
+++ Library.Web/Program.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Library.Database;
+using Library.Database.Seed;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace Library.Web
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<LibraryContext>();
+                    Seeder.Init(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
