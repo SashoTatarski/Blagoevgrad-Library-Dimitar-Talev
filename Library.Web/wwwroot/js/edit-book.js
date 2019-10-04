fdc4467f22c2263e55060b9e37b1a093d6689716
SashoTatarski <sasho56@hotmail.com>
2019-10-04 15:36:00 +0300

    EditBook modals to be fixed!

---
+++ Library.Web/wwwroot/js/edit-book.js
@@ -0,0 +1,72 @@
+﻿$('#add-author-edit').click(function () {
+    $('#create-author-modal-edit').modal('show');
+    console.log('Success');
+});
+
+$('#create-author-edit').click(function () {
+    const authorNameValue = $('#new-author-name-edit').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/bookmanagement/addauthorrest',
+        data: { authorName: authorNameValue },
+        success: function (response) {
+            $('#create-author-modal-edit').modal('hide');
+
+            const option = $(`<option value="${response.id}">${response.name}</option>`);
+
+            $('#authors-selectlist-edit').append(option);
+
+            option.attr('selected', 'selected');
+        }
+    });
+});
+
+
+
+$('#add-publisher').click(function () {
+
+    $('#create-publisher-modal').modal('show');
+});
+
+$('#create-publisher').click(function () {
+    const authorPublisherValue = $('#new-publisher-name').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/bookmanagement/addpublisherrest',
+        data: { publisherName: authorPublisherValue },
+        success: function (response) {
+
+            $('#create-publisher-modal').modal('hide');
+
+            const option1 = $(`<option value="${response.id}">${response.name}</option>`);
+            $('#publisher-selectlist').append(option1);
+
+            option1.attr('selected', 'selected');
+        }
+    });
+});
+
+$('#add-genre').click(function () {
+    $('#create-genre-modal').modal('show');
+});
+
+$('#create-genre').click(function () {
+    const genreValue = $('#new-genre-name').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/bookmanagement/addgenrerest',
+        data: { genreName: genreValue },
+        success: function (response) {
+
+            $('#create-genre-modal').modal('hide');
+
+            const option1 = $(`<option value="${response.id}">${response.name}</option>`);
+            $('#genre-selectlist').append(option1);
+
+            option1.attr('selected', 'selected');
+        }
+    });
+});
