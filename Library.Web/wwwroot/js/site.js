1b14faac1ac2d565a2266c5508d5009161a56eb9
SashoTatarski <sasho56@hotmail.com>
2019-10-09 17:12:02 +0300

    Search with AJAX started

--- Library.Web/wwwroot/js/site.js
+++ Library.Web/wwwroot/js/site.js
@@ -1,2 +1,15 @@
-﻿
+﻿//$('#search-index-form').on('submit', function (event) {
+//    event.preventDefault();
+
+//    const data = $(this).serialize();
+//    console.log(data);
+
+//    $.ajax({
+//        type: 'POST',
+//        url: '/SearchBooks/SearchResults',
+//        data: { authorName: data },
+//        success: ????
+//        }
+//    });
+//});
     
